5ce264b625c3ce9d7d21135c795a8638145e944a
mr-robotyaga <72809653+mr-robotyaga@users.noreply.github.com>
2020-10-31 15:32:41 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let Width = document.getElementById('width');
-let Height = document.getElementById('height');
-Width.textContent = window.outerWidth + 'px';
-Height.textContent = window.outerHeight + 'px';
-window.onresize = function() {
-    Width.textContent = window.outerWidth + 'px';
-    Height.textContent = window.outerHeight + 'px';
-}
